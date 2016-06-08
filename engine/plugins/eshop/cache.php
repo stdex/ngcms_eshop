a1efe62073df34d35aa56d7e858f300fa788d9fe
ROZARD <rozard@list.ru>
2016-06-09 00:35:25 +0400

    Add files via upload
    Исправление кодировки.

--- engine/plugins/eshop/cache.php
+++ engine/plugins/eshop/cache.php
@@ -156,6 +156,7 @@ function generate_features_cache($load = false)
         {
             $foptions = array();
             $foptions = json_decode($row['foptions'],true);
+			$foptions = array_map( function ($array){ return iconv('UTF-8', 'Windows-1251', $array); }, $foptions );
             $row['foptions'] = $foptions;
             $features_tEntry[] = $row;
         }
