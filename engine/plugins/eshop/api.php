11ee439f92cb67a4519c2034fcf7cfd976fe9770
stdex <sergey@rostunov.com>
2017-09-22 05:06:15 +0300

    API

--- engine/plugins/eshop/api.php
+++ engine/plugins/eshop/api.php
@@ -39,8 +39,10 @@ class ApiEshop
     {
         $conditions = [];
         $this->addDateTimeCondition($conditions);
+
         $orders = $this->getOrders($conditions);
 
+        $this->encodeUtf8Array($orders);
         $results = ['data' => $orders, 'status' => 'OK'];
         $this->renderResults($results);
     }
@@ -49,6 +51,10 @@ class ApiEshop
     {
         $conditions = [];
         $this->addDateTimeCondition($conditions);
+        $conditions = [];
+        if (!empty($this->control['order_id'])) {
+            $conditions[] = 'o.id = ' . (int)$this->control['order_id'];
+        }
         $orders = array_keys($this->getOrders($conditions));
         $positions = $this->getProducts($orders);
 
@@ -61,9 +67,10 @@ class ApiEshop
         global $mysql;
 
         foreach ($mysql->select("SELECT * FROM ".prefix."_eshop_features ORDER BY position, id") as $frow) {
-            $options = $frow;
+            $options[] = $frow;
         }
 
+        $this->encodeUtf8Array($options);
         $results = ['data' => $options, 'status' => 'OK'];
         $this->renderResults($results);
     }
@@ -77,6 +84,7 @@ class ApiEshop
         }
 
         $variants = $this->getVariants($conditions);
+        $this->encodeUtf8Array($variants);
 
         $results = ['data' => $variants, 'status' => 'OK'];
         $this->renderResults($results);
@@ -86,20 +94,40 @@ class ApiEshop
     {
         global $mysql;
 
+        $output = [];
         $params = $this->getParams();
 
-        foreach ($params as $item) {
+        foreach ($params as $id => $item) {
+
+            $checked = $this->checkRequiredParams($item, ['order_id', 'status']);
+            if (!$checked) {
+                $output[$id] = ['id' => NULL, 'status' => 'error', 'message' => 'Please fill out all required fields'];
+                continue;
+            }
+
             $order_id = $item['order_id'];
             $status = $item['status'];
 
-            if(!empty($order_id)) {
-                $mysql->query(
-                    "UPDATE ".prefix."_eshop_orders SET paid = ".(int)$status." WHERE id = ".(int)$order_id
-                );
+            $checked = $this->checkOrderID($order_id);
+            if (!$checked) {
+                $output[$id] = ['id' => NULL, 'status' => 'error', 'message' => 'Item with this ID does not exist'];
+                continue;
             }
+
+            if (!in_array($status, ["0", "1"])) {
+                $output[$id] = ['id' => NULL, 'status' => 'error', 'message' => 'Incorrect status'];
+                continue;
+            }
+
+            $mysql->query(
+                "UPDATE ".prefix."_eshop_orders SET paid = ".(int)$status." WHERE id = ".(int)$order_id
+            );
+            $output[$id] = ['id' => $order_id, 'status' => 'OK'];
+
         }
 
-        $results = ['data' => [], 'status' => 'OK'];
+        $this->encodeUtf8Array($output);
+        $results = ['data' => $output, 'status' => 'OK'];
         $this->renderResults($results);
     }
 
@@ -107,6 +135,7 @@ class ApiEshop
     {
         global $mysql;
 
+        $output = [];
         $params = $this->getParams();
 
         $mapParams = [
@@ -119,16 +148,24 @@ class ApiEshop
             'sku' => 'sku',
         ];
 
-        foreach ($params as $item) {
+        $required = ['update' => ['id'], 'insert' => ['product_id']];
+
+        foreach ($params as $id => $item) {
             if (!empty($item['id'])) {
-                $vnames = [];
-                foreach ($item as $id => $value) {
-                    if (in_array($id, ['id', 'product_id'])) {
-                        continue;
-                    }
-                    $key = $mapParams[$id];
-                    $vnames[] = $key.' = '.db_squote($value);
+                $checked = $this->checkRequiredParams($item, $required['update']);
+                if (!$checked) {
+                    $output[$id] = ['id' => NULL, 'status' => 'error', 'message' => 'Please fill out all required fields'];
+                    continue;
                 }
+                $checked = $this->checkVariantID($item['id']);
+                if (!$checked) {
+                    $output[$id] = ['id' => NULL, 'status' => 'error', 'message' => 'Item with this ID does not exist'];
+                    continue;
+                }
+                unset($item['product_id']);
+                $params = $this->getParamsArray($item, $mapParams);
+                $vnames = $this->generateUpdateArray($params);
+
                 if (!empty($vnames)) {
                     $mysql->query(
                         'UPDATE '.prefix.'_eshop_variants SET '.implode(
@@ -136,11 +173,37 @@ class ApiEshop
                             $vnames
                         ).' WHERE id = \''.(int)$item['id'].'\' '
                     );
+                    $output[$id] = ['id' => $item['id'], 'status' => 'OK'];
+                }
+            } else {
+                $checked = $this->checkRequiredParams($item, $required['insert']);
+                if (!$checked) {
+                    $output[$id] = ['id' => NULL, 'status' => 'error', 'message' => 'Please fill out all required fields'];
+                    continue;
+                }
+                $checked = $this->checkProductID($item['product_id']);
+                if (!$checked) {
+                    $output[$id] = ['id' => NULL, 'status' => 'error', 'message' => 'Item with this ID does not exist'];
+                    continue;
+                }
+
+                $params = $this->getParamsArray($item, $mapParams);
+                $vnames = $this->generateInsertArray($params);
+                if (!empty($vnames)) {
+                    $mysql->query(
+                        "INSERT INTO ".prefix."_eshop_variants (".implode(",", array_keys($vnames)).") values (".implode(
+                            ",",
+                            array_values($vnames)
+                        ).")"
+                    );
+                    $qid = $mysql->lastid('eshop_variants');
+                    $output[$id] = ['id' => $qid, 'status' => 'OK'];
                 }
             }
         }
 
-        $results = ['data' => [], 'status' => 'OK'];
+        $this->encodeUtf8Array($output);
+        $results = ['data' => $output, 'status' => 'OK'];
         $this->renderResults($results);
     }
 
@@ -149,6 +212,7 @@ class ApiEshop
 
         global $mysql;
 
+        $output = [];
         $params = $this->getParams();
 
         $mapParams = [
@@ -159,16 +223,22 @@ class ApiEshop
             'vendor_code' => 'code',
         ];
 
-        foreach ($params as $item) {
+        $required = ['update' => ['id'], 'insert' => ['name']];
+
+        foreach ($params as $id => $item) {
             if (!empty($item['id'])) {
-                $vnames = [];
-                foreach ($item as $id => $value) {
-                    if ('id' == $id) {
-                        continue;
-                    }
-                    $key = $mapParams[$id];
-                    $vnames[] = $key.' = '.db_squote($value);
+                $checked = $this->checkRequiredParams($item, $required['update']);
+                if (!$checked) {
+                    $output[$id] = ['id' => NULL, 'status' => 'error', 'message' => 'Please fill out all required fields'];
+                    continue;
                 }
+                $checked = $this->checkProductID($item['id']);
+                if (!$checked) {
+                    $output[$id] = ['id' => NULL, 'status' => 'error', 'message' => 'Item with this ID does not exist'];
+                    continue;
+                }
+                $params = $this->getParamsArray($item, $mapParams);
+                $vnames = $this->generateUpdateArray($params);
                 if (!empty($vnames)) {
                     $mysql->query(
                         'UPDATE '.prefix.'_eshop_products SET '.implode(
@@ -176,11 +246,38 @@ class ApiEshop
                             $vnames
                         ).' WHERE id = \''.(int)$item['id'].'\' '
                     );
+                    $output[$id] = ['id' => $item['id'], 'status' => 'OK'];
+                }
+            } else {
+                $checked = $this->checkRequiredParams($item, $required['insert']);
+                if (!$checked) {
+                    $output[$id] = ['id' => NULL, 'status' => 'error', 'message' => 'Please fill out all required fields'];
+                    continue;
+                }
+                $checked = $this->checkProductURL(['name' => $item['name']]);
+                if ($checked) {
+                    $output[$id] = ['id' => NULL, 'status' => 'error', 'message' => 'Item with the same URL already exist'];
+                    continue;
+                }
+                $item['url'] = $this->generateURLbyName($item['name']);
+                $mapParams['url'] = 'url';
+                $params = $this->getParamsArray($item, $mapParams);
+                $vnames = $this->generateInsertArray($params);
+                if (!empty($vnames)) {
+                    $mysql->query(
+                        "INSERT INTO ".prefix."_eshop_products (".implode(",", array_keys($vnames)).") values (".implode(
+                            ",",
+                            array_values($vnames)
+                        ).")"
+                    );
+                    $qid = $mysql->lastid('eshop_products');
+                    $output[$id] = ['id' => $qid, 'status' => 'OK'];
                 }
             }
         }
 
-        $results = ['data' => [], 'status' => 'OK'];
+        $this->encodeUtf8Array($output);
+        $results = ['data' => $output, 'status' => 'OK'];
         $this->renderResults($results);
     }
 
@@ -188,6 +285,7 @@ class ApiEshop
 
         global $mysql;
 
+        $output = [];
         $params = $this->getParams();
 
         $mapParams = [
@@ -196,29 +294,62 @@ class ApiEshop
             'value' => 'value'
         ];
 
-        foreach ($params as $item) {
+        $required = ['update' => ['id', 'product_id', 'value'], 'insert' => ['id', 'product_id', 'value']];
+
+        foreach ($params as $id => $item) {
             if (!empty($item['id'])) {
-                $vnames = [];
-                foreach ($item as $id => $value) {
-                    if ('id' == $id) {
-                        continue;
-                    }
-                    $key = $mapParams[$id];
-                    $vnames[] = $key.' = '.db_squote($value);
+
+                $checked = $this->checkRequiredParams($item, $required['update']);
+                if (!$checked) {
+                    $output[$id] = ['id' => NULL, 'status' => 'error', 'message' => 'Please fill out all required fields'];
+                    continue;
+                }
+                $checked = $this->checkFeatureID($item['id']);
+                if (!$checked) {
+                    $output[$id] = ['id' => NULL, 'status' => 'error', 'message' => 'Item with this ID does not exist'];
+                    continue;
                 }
 
-                if (!empty($vnames)) {
-                    $mysql->query(
-                        'UPDATE '.prefix.'_eshop_options SET '.implode(
-                            ', ',
-                            $vnames
-                        ).' WHERE '.$mapParams["id"].' = \''.(int)$item['id'].'\' '
-                    );
+                $checked = $this->checkProductID($item['product_id']);
+                if (!$checked) {
+                    $output[$id] = ['id' => NULL, 'status' => 'error', 'message' => 'Item with this ID does not exist'];
+                    continue;
+                }
+
+                $checked = $this->checkOptionID($item['id'], $item['product_id']);
+                if ($checked) {
+                    $feature_id = $item['id'];
+                    $product_id = $item['product_id'];
+                    unset($item['id'], $item['product_id']);
+                    $params = $this->getParamsArray($item, $mapParams);
+                    $vnames = $this->generateUpdateArray($params);
+                    if (!empty($vnames)) {
+                        $mysql->query(
+                            'UPDATE '.prefix.'_eshop_options SET '.implode(
+                                ', ',
+                                $vnames
+                            ).' WHERE '.$mapParams["id"].' = \''.(int)$feature_id.'\' AND '.$mapParams["product_id"].' = \''.(int)$product_id.'\''
+                        );
+                        $output[$id] = ['id' => NULL, 'status' => 'OK'];
+                    }
+                } else {
+                    $params = $this->getParamsArray($item, $mapParams, true);
+                    $vnames = $this->generateInsertArray($params);
+                    if (!empty($vnames)) {
+                        $mysql->query(
+                            "INSERT INTO ".prefix."_eshop_options (".implode(",", array_keys($vnames)).") values (".implode(
+                                ",",
+                                array_values($vnames)
+                            ).")"
+                        );
+                        $output[$id] = ['id' => NULL, 'status' => 'OK'];
+                    }
                 }
             }
         }
 
-        $results = ['data' => [], 'status' => 'OK'];
+        $this->encodeUtf8Array($output);
+        $results = ['data' => $output, 'status' => 'OK'];
         $this->renderResults($results);
     }
 
@@ -283,7 +414,7 @@ class ApiEshop
         $orders = [];
         $fSort = " ORDER BY o.id";
         $sqlQ = "
-        SELECT * 
+        SELECT * , o.id as order_id
         FROM ".prefix."_eshop_orders o 
         LEFT JOIN ".prefix."_users u ON o.author_id = u.id ".(count($conditions) ? "WHERE ".implode(
                     " AND ",
@@ -291,7 +422,7 @@ class ApiEshop
                 ) : "").$fSort;
 
         foreach ($mysql->select($sqlQ) as $row) {
-            $orders[$row['id']] = $row;
+            $orders[$row['order_id']] = $row;
         }
 
         return $orders;
@@ -306,6 +437,7 @@ class ApiEshop
                     " AND ",
                     $conditions
                 ) : "").$fSort;
+
         foreach ($mysql->select($sqlQ) as $row) {
             $variants[] = $row;
         }
@@ -343,9 +475,184 @@ class ApiEshop
         }
     }
 
+    public function getParamsArray($item, $mapParams, $includeID = false)
+    {
+        $params = [];
+        foreach ($item as $id => $value) {
+            if (('id' == $id) && !$includeID) {
+                continue;
+            }
+            $key = $mapParams[$id];
+            $params[$key] = iconv("utf-8", "windows-1251", $value);
+        }
+
+        return $params;
+    }
+
+    public function generateUpdateArray($params)
+    {
+        $output = [];
+        foreach ($params as $key => $value) {
+            $output[] = $key.' = '.db_squote($value);
+        }
+
+        return $output;
+    }
+
+    public function generateInsertArray($params)
+    {
+        $output = [];
+        foreach ($params as $key => $value) {
+            $output[$key] = db_squote($value);
+        }
+
+        return $output;
+    }
+
+    public function checkRequiredParams($item, $required)
+    {
+
+        if (empty(array_intersect_key($item, array_flip($required)))) {
+            return false;
+        }
+
+        foreach ($item as $id => $param) {
+            if (in_array($id, $required)) {
+                if ($param == "") {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public function checkProductURL($SQL)
+    {
+        global $mysql, $parse;
+
+        if (empty($SQL['url'])) {
+            $SQL['url'] = $this->generateURLbyName($SQL['name']);
+        }
+
+        if ($SQL["url"]) {
+            if (is_array(
+                $mysql->record(
+                    "SELECT id FROM ".prefix."_eshop_products WHERE url = ".db_squote($SQL["url"])." LIMIT 1"
+                )
+            )) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function checkProductID($id)
+    {
+        global $mysql;
+
+        if ($id) {
+            if (is_array(
+                $mysql->record(
+                    "SELECT id FROM ".prefix."_eshop_products WHERE id = ".(int)$id." LIMIT 1"
+                )
+            )) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function checkVariantID($id)
+    {
+        global $mysql;
+
+        if ($id) {
+            if (is_array(
+                $mysql->record(
+                    "SELECT id FROM ".prefix."_eshop_variants WHERE id = ".(int)$id." LIMIT 1"
+                )
+            )) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function checkFeatureID($id)
+    {
+        global $mysql;
+
+        if ($id) {
+            if (is_array(
+                $mysql->record(
+                    "SELECT id FROM ".prefix."_eshop_features WHERE id = ".(int)$id." LIMIT 1"
+                )
+            )) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function checkOptionID($id, $product_id)
+    {
+        global $mysql;
+
+        if ($id) {
+            if (is_array(
+                $mysql->record(
+                    "SELECT * FROM ".prefix."_eshop_options WHERE feature_id = ".(int)$id." AND product_id = ".(int)$product_id."  LIMIT 1"
+                )
+            )) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function checkOrderID($id)
+    {
+        global $mysql;
+
+        if ($id) {
+            if (is_array(
+                $mysql->record(
+                    "SELECT id FROM ".prefix."_eshop_orders WHERE id = ".(int)$id." LIMIT 1"
+                )
+            )) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function generateURLbyName($name)
+    {
+        global $parse;
+
+        return strtolower($parse->translit($name, 1, 1));
+    }
+
+    public function encodeUtf8Array(&$items)
+    {
+        foreach ($items as $itemID => $item) {
+            foreach ($item as $id => $param) {
+                $items[$itemID][$id] = iconv("windows-1251", "utf-8", $param);
+            }
+        }
+    }
+
     public function getParams()
     {
-        $params = json_decode($_POST['data'], 1);
+        $data = file_get_contents("php://input");
+        $params = json_decode($data, 1);
 
         return $params;
     }
